bca533907ac9151c7fd52714756bcc91ecb1e601
Hannah <h.smythosbourne@gmail.com>
2016-09-20 16:26:59 +0100

    Print welcome message

---
+++ test/com/biblioteca/MessageTest.java
@@ -0,0 +1,35 @@
+package com.biblioteca;
+
+
+import com.biblioteca.Message;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by Robin on 20/09/2016.
+ */
+public class MessageTest {
+
+    private Message message;
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void beforeEach(){
+        outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        message = new Message();
+    }
+
+    @Test
+    public void printsMessage() {
+        message.print();
+        assertEquals("Welcome to biblioteca!", outputStream.toString());
+    }
+}
